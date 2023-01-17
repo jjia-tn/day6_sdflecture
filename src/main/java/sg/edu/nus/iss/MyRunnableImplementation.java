0d27cac004b1fad98b122e503aef7e09b0face70
jjia-tn <jjia.tn@gmail.com>
2023-01-17 11:03:02 +0800

    Basic Threading example

---
+++ src/main/java/sg/edu/nus/iss/MyRunnableImplementation.java
@@ -0,0 +1,22 @@
+package sg.edu.nus.iss;
+
+public class MyRunnableImplementation implements Runnable {
+
+    private String taskName;
+
+    public MyRunnableImplementation() {
+    }
+
+    public MyRunnableImplementation(String taskName) {
+        this.taskName = taskName;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + "\t Runnable... " + i);
+        }
+        
+    }
+    
+}
